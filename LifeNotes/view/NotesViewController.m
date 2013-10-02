e897ed4d9c02c651705b54f17ff11ccaf2e1f079
ahmemon <ahmemon@gmail.com>
2013-10-01 18:22:39 -0700

    get rid of the status bar

--- LifeNotes/view/NotesViewController.m
+++ LifeNotes/view/NotesViewController.m
@@ -39,12 +39,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+- (BOOL) prefersStatusBarHidden
+{
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
