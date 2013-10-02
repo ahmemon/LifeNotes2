05fd5665b699aea71afd4ac0038f31bf8e81ac61
ahmemon <ahmemon@gmail.com>
2013-10-01 19:00:09 -0700

    remove part of a file, want to produce a conflict

--- LifeNotes/AppDelegate.m
+++ LifeNotes/AppDelegate.m
@@ -27,7 +27,7 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    // Called as part of the transition from the background to the inactive state; 
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
